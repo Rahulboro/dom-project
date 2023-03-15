41dfd1ebe3e57fbe4da3d6f1c39ec5182eddfdc1
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-03-15 23:45:23 +0530

    correction in no1

--- 01Accordion/faq.js
+++ 01Accordion/faq.js
@@ -1,49 +1,37 @@
+const faqlist = [
+  {
+    id: 1,
+    question: "Who are we?",
+    answer:
+      "We enable upscaling careers through flexible, interactive and collaborative learning. We believe in building learning communities by bringing together mentors, young minds, and creators.",
+  },
+  {
+    id: 2,
+    question: "What do we do?",
+    answer:
+      "Building learning communities with Our Affordable & Competent Courses",
+  },
+  {
+    id: 3,
+    question: "Are the community classes boring?",
+    answer: "No, not at all!!!",
+  },
+];
 
-let div = document.createElement('div')
-let hTwo = document.createElement('h3')
-let p = document.createElement('p')
-//one faq
-div.setAttribute('class', 'faq')
-hTwo.append("Who Are We ?")
-div.appendChild(hTwo)
-document.body.append(div)
+const accordianBody = document.querySelector(".accordian_body")
+const faqs = [];
 
-
-
-
-
-// [
-//     id: 1,
-//     question: "Who are we?",
-//     answer:
-//       "We enable upscaling careers through flexible, interactive and collaborative learning. We believe in building learning communities by bringing together mentors, young minds, and creators.",
-//   {
-//     id: 2,
-//     question: "What do we do?",
-//     answer:
-//       "Building learning communities with Our Affordable & Competent Courses",
-//   },
-//   {
-//     id: 3,
-//     question: "Are the community classes boring?",
-//     answer: "No, not at all!!!",
-//   },
-// ];
-
-// const accordianBody;
-// const faqs = [];
-
-// function showFaq() {
+function showFaq() {
   
-// }
+}
 
-// function createFaq() {
+function createFaq() {
   
-// }
+}
 
-// function btnStatusUpdate() {
+function btnStatusUpdate() {
   
-// }
+}
 
 
 
