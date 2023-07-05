ebd22814963cafd99ee9e1117156b53c6179d299
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-07-05 23:24:23 +0530

    bringing api

--- 06JokesGenerator - API/script.js
+++ 06JokesGenerator - API/script.js
@@ -1,1 +1,16 @@
-const jokeid = document.querySelector('#joke')
+const url = 'https://dad-jokes.p.rapidapi.com/random/joke';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f66624530cmshd7e361d254eb304p19c792jsncf3ac59211eb',
+		'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
