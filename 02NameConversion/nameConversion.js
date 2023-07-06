bc26c81ea9464500cbf6765ea4ac94eac426c44b
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-07-06 23:50:18 +0530

    script

--- 02NameConversion/nameConversion.js
+++ 02NameConversion/nameConversion.js
@@ -1,13 +1,13 @@
 // Defining ID
 
-// let textArea = document.querySelector('text');
-// let convertBtn = document.querySelector('convert-btn');
-// let camelCase = document.querySelector('camel-case')
-// let pascalCase = document.querySelector('pascal-case')
-// let snakeCase = document.querySelector('snake-case')
-// let screamingSnakeCase = document.querySelector('screaming-snake-case')
-// let kebabCase = document.querySelector('kebab-case')
-// let screamingKebabCase = document.querySelector('screaming-kebab-case')
+let textArea = document.querySelector('text');
+let convertBtn = document.querySelector('convert-btn');
+let camelCase = document.querySelector('camel-case')
+let pascalCase = document.querySelector('pascal-case')
+let snakeCase = document.querySelector('snake-case')
+let screamingSnakeCase = document.querySelector('screaming-snake-case')
+let kebabCase = document.querySelector('kebab-case')
+let screamingKebabCase = document.querySelector('screaming-kebab-case')
 
 
 // adding event listener 
@@ -26,16 +26,16 @@ function input() {
         }
         camel_Case()
     
-    document.getElementById('pascal-case').innerText = gettext.
-    toUpperCase();
-    document.getElementById('snake-case').innerText = gettext.
-    toUpperCase();
-    document.getElementById('screaming-snake-case').innerText = gettext.
-    toUpperCase();
-    document.getElementById('kebab-case').innerText = gettext.
-    toUpperCase();
-    document.getElementById('screaming-kebab-case').innerText = gettext.
-    toUpperCase();
+    // document.getElementById('pascal-case').innerText = gettext.
+    // toUpperCase();
+    // document.getElementById('snake-case').innerText = gettext.
+    // toUpperCase();
+    // document.getElementById('screaming-snake-case').innerText = gettext.
+    // toUpperCase();
+    // document.getElementById('kebab-case').innerText = gettext.
+    // toUpperCase();
+    // document.getElementById('screaming-kebab-case').innerText = gettext.
+    // toUpperCase();
     }
     )
 }
